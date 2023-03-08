ddd8ec86a6cbfee3b8cddc947af675c8686dd625
fasilkp <fasilkp314@gmail.com>
2023-03-08 13:48:17 +0530

    login page ui created

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 // import './App.css';
-import UserHome from './component/UserHome/UserHome';
+import UserHome from "./component/UserHome/UserHome";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserLogin from "./component/UserLogin/UserLogin";
 
 function App() {
   return (
-    <div className="App">
-      <UserHome></UserHome>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<UserHome />}></Route>
+          <Route path="/login" element={<UserLogin />}></Route>
+        
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
- 
