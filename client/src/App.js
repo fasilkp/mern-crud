43200de7ae715789690714c2af14acc3b19a4882
fasilkp <fasilkp314@gmail.com>
2023-03-08 14:31:22 +0530

    admin login ui created

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import UserHome from "./component/UserHome/UserHome";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserLogin from "./component/UserLogin/UserLogin";
 import UserRegister from "./component/UserRegister/UserRegister";
+import AdminLogin from "./component/AdminLogin/AdminLogin";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<UserHome />}></Route>
           <Route path="/login" element={<UserLogin />}></Route>
           <Route path="/register" element={<UserRegister />}></Route>
+          <Route path="/admin/login" element={<AdminLogin />}></Route>
         
         </Routes>
       </div>
