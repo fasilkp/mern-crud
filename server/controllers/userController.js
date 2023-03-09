8bde1a06e9f64f84c722fb3085c4011ef8d6c237
fasilkp <fasilkp314@gmail.com>
2023-03-09 10:22:47 +0530

    user login created

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -5,12 +5,12 @@ import jwt from 'jsonwebtoken'
 
 var salt = bcrypt.genSaltSync(10);
 
-export async function UserRegister(req, res){
+export async function userRegister(req, res){
         try
         {
-            const {name, email, password}=req.body;
+            const {name, email, password, about, proffession}=req.body;
             const hashPassword = bcrypt.hashSync(password, salt);
-            const newUser = new UserModel({name, email,password:hashPassword})
+            const newUser = new UserModel({name, email,password:hashPassword, about, proffession})
             await newUser.save();
             res.json({success:true})
         }
@@ -18,4 +18,35 @@ export async function UserRegister(req, res){
             res.json({success:false, error:err})
             console.log(err);
         }
+}
+
+export async function userLogin(req, res){
+    try
+    {
+        const {email, password}=req.body;
+        const user=await UserModel.findOne({email})
+        if(!user) 
+            return res.json({success:false, error:"No email found"})
+        const userValid=bcrypt.compareSync(password, user.password);
+        if(!userValid) 
+            return res.json({success:false, error:"Password wrong"})
+        const token=jwt.sign(
+            {
+                name:user.name,
+                id:user._id
+            }, 
+            process.env.JWT_SECRET_KEY
+        )
+        // const exp= new Date()+ 1000*60;
+        return res.cookie("token", token, {
+                httpOnly: true,
+                secure: true,
+                maxAge: 1000 * 60 * 60 * 24 * 7,
+                sameSite: "none",
+            }).json({success:true, user:user._id})
+    }
+    catch(err){
+        res.json({success:false,message:"server error", error:err})
+        console.log(err);
+    }
 }
